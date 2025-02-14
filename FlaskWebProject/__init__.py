0c4891cd7022ef0c3055364bf3d4c6ac1c8e51a9
pearlk02 <pearl.khotsombath@chevron.com>
2025-02-14 10:15:10 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,18 +12,18 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 # Setting logging level to info
-app.logger.setLevel(logging.INFO)
-# Creating stream handler for logging
-streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.INFO)
-# Adding handler to app.logger object
-app.logger.addHandler(streamHandler)
+# app.logger.setLevel(logging.INFO)
+# # Creating stream handler for logging
+# streamHandler = logging.StreamHandler()
+# streamHandler.setLevel(logging.INFO)
+# # Adding handler to app.logger object
+# app.logger.addHandler(streamHandler)
 
-# Log messages for different logging levels
-app.logger.info('Info level: No issue.')
-app.logger.warning('Warning level: Warning occurred.')
-app.logger.error('Error level: Error occurred.')
-app.logger.critical('Critical level: Critical error occurred.')
+# # Log messages for different logging levels
+# app.logger.info('Info level: No issue.')
+# app.logger.warning('Warning level: Warning occurred.')
+# app.logger.error('Error level: Error occurred.')
+# app.logger.critical('Critical level: Critical error occurred.')
 
 Session(app)
 db = SQLAlchemy(app)
