a813ffab1f5afe84c273c0ab95868266ac06d6a8
pearlk02 <pearl.khotsombath@chevron.com>
2025-02-14 10:15:40 -0600

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -135,7 +135,7 @@ def _build_msal_app(cache=None, authority=None):
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
-    return _build_msal_app(authority=authority).get_authorization_request_url(
-        scopes,
-        state=state,
-        redirect_uri=url_for('authorized', _external=True, _scheme='https'))
+    # return _build_msal_app(authority=authority).get_authorization_request_url(
+    #     scopes,
+    #     state=state,
+    #     redirect_uri=url_for('authorized', _external=True, _scheme='https'))
