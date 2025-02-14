c645340beb68d441ebb64d00dcfe10efc458f783
pearlk02 <pearl.khotsombath@chevron.com>
2025-02-14 10:56:11 -0600

    Update cache again in views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -117,15 +117,14 @@ def logout():
 def _load_cache():
     # TODO: Load the cache from `msal`, if it exists
     cache = msal.SerializableTokenCache()
+    if session.get("token_cache"):
+        cache.deserialize(session["token_cache"])
     return cache
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
     if cache.has_state_changed:
-        _build_msal_app(cache=cache)
-        cache.has_state_changed = False
-    else:
-        pass
+        session["token_cache"] = cache.serialize()
 
 def _build_msal_app(cache=None, authority=None):
     # TODO: Return a ConfidentialClientApplication
